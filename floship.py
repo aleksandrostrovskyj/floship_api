07952d69e571aae4ae029bcc83b5d47094a91a0d
aleksandrostrovskyj <aleksandr.ostrovskyj@gmail.com>
2020-02-12 12:18:27 +0200

    Bridge Pattern - 1
    moved the decorator into the separate module decorators.py
    
    floship.py
      # implement bridge pattern

--- floship.py
+++ floship.py
@@ -1,85 +1,65 @@
 import json
-import pickle
 import logging
 import requests
-import functools
 from settings import config
-from datetime import datetime
+from abc import ABC, abstractmethod
+from decorators import ResponseHandler
 
 logging.basicConfig(format='%(asctime)s - %(cor_func_name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
-class ResponseHandler:
-    """
-    Class implementing a response processing decorator
-    """
-    @classmethod
-    def handler(cls, func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            log_extra = {'cor_func_name': func.__name__}
-            try:
-                logging.info('Send request.', extra=log_extra)
-                response = func(*args, **kwargs)
-                logging.info('Check status.', extra=log_extra)
-                response.raise_for_status()
-            except requests.exceptions.HTTPError as http_error:
-                logging.warning(f'{http_error} - Issue with request', extra=log_extra)
-            except Exception as exc:
-                logging.warning(f'{exc} - issue in program', extra=log_extra)
-            else:
-                logging.info(f'OK - {response}', extra=log_extra)
-                return response
-        return wrapper
+"""
+Bridge Pattern
+"""
 
 
-class FloshipBaseApi:
+class Floship:
     """
-    Base class
+    Abstraction class
     """
+    def __init__(self, implementation) -> None:
+        self.implementation = implementation
+
+    @ResponseHandler.handler
+    def make_request(self, *args, **kwargs):
+        return self.implementation.make_request(*args, **kwargs)
+
+
+class Implementation(ABC):
+    """
+
+    """
+    @abstractmethod
+    def make_request(self, method, api_url, params):
+        pass
+
+
+class FloshipAPI(Implementation):
+    """
+    Implementation of the Floship API
+    https://admin.floship.com/developers
+    """
+
     config = config["floship_api"]
     headers = {
         'Authorization': f'Token {config["token"]}',
         'Content-Type': 'application/json',
         'Accept': 'application/json'
     }
-    main_url = 'https://admin.floship.com/api/v1/{}'
+    main_url = 'https://admin.floship.com/api/v1/'
 
     def __init__(self):
         self.session = requests.Session()
         self.session.headers.update(self.headers)
+        self.url = f'{self.main_url}'
 
-    @ResponseHandler.handler
-    def make_request(self, *args, **kwargs):
-        request_url = self.main_url.format(kwargs.pop('api_url'))
-        return self.session.request(*args, url=request_url, headers=self.headers, **kwargs)
+    def make_request(self, method, api_url, params):
+        return self.session.request(method=method, url=f'{self.url}{api_url}', params=params)
 
 
-class FloshipAPI(FloshipBaseApi):
+class FloshipSession(Implementation):
     """
-    Class for each API entity realizing
-    """
-    def list_orders(self, params={}):
-        params = {
-            'page': 1,
-            'limit': 50,
-            **params
-        }
-
-        while True:
-            response = self.make_request(method='GET', api_url='orders/', params=params)
-            yield response
-
-            if not response.json()['next']:
-                # If no pagination - exit loop
-                break
-
-            params['page'] += 1
-
-
-class FloshipBaseSession:
-    """
-    Base class for Session requests (internal Floship API)
+    Implementation of the internal Floship API
     Main reason: not all information exists in regular API
     Should only be used for cost information retrieving.
     """
@@ -105,69 +85,12 @@ class FloshipBaseSession:
 
     @ResponseHandler.handler
     def login(self):
+        """
+        Method to retrieve session token for further requests
+        :return:
+        """
         return self.session.request('POST', url=self.login_url, data=json.dumps(self.payload))
 
-    @ResponseHandler.handler
-    def make_request(self, *args, **kwargs):
-        return self.session.request(*args, **kwargs)
+    def make_request(self, method, url, data=None, params=None):
+        return self.session.request(method=method, url=url, data=data, params=params)
 
-
-class FloshipSession(FloshipBaseSession):
-    """
-    Class to retrieve data via internal Floship API
-    """
-    def list_orders(self, date_from: datetime = ''):
-        """
-        Main purpose - retreive cost information
-        :param date_from: start date of order creation
-        :return: <class 'requests.models.Response'>
-        """
-        url = 'https://admin.floship.com/internal_api/orders/'
-        params = {
-            'order_type__in': 'stock,crossdocking,parcel_forwarding',
-            'sort_by': '-create_date',
-            'page': 1
-        }
-
-        if date_from:
-            # Date to string conversion
-            str_date_from = date_from.strftime('%Y-%m-%dT00:00:00')
-            params['create_date_from'] = str_date_from
-
-        while True:
-            response = self.make_request(method='GET', url=url, params=params)
-            yield response
-
-            if not response.json()['next']:
-                # If no pagination - exit loop
-                break
-
-            params['page'] += 1
-
-        self.session.close()
-
-
-def test_api():
-    f = FloshipAPI()
-    count = 0
-    for page in f.list_orders():
-        count += 1
-        json_data = json.dumps(page.json(), indent=3)
-        with open(f'test_results\\api_floship_{count}.json', 'w') as f:
-            f.writelines(json_data)
-
-        with open(f'test_results\\api_floship_{count}.pickle', 'wb') as f:
-            pickle.dump(file=f, obj=page)
-
-
-def test_session():
-    f = FloshipSession()
-    count = 0
-    for page in f.list_orders():
-        count += 1
-        json_data = json.dumps(page.json(), indent=3)
-        with open(f'test_results\\session_orders_response_{count}.pickle', 'wb') as f:
-            pickle.dump(file=f, obj=page)
-
-        with open(f'test_results\\session_orders_response_{count}.json', 'w') as f:
-            f.writelines(json_data)
